4660f8ceb4bb752413aa5f9a4b5f9a80225a68f0
Joojjuuk <joojjuuk@gmail.com>
2025-06-15 11:25:23 -0300

    Splash Screen com transição para página principal - Imagens

---
+++ lib/view/home/sus-news/sustainability.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Sustainability extends StatefulWidget {
+  const Sustainability({super.key});
+
+  @override
+  State<Sustainability> createState() => _SustainabilityState();
+}
+
+class _SustainabilityState extends State<Sustainability> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+
+  }
+}
---
+++ lib/view/splashscreen/splash.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:appescolar/view/home/sus-news/sustainability.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 2500), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Sustainability()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/logo/splashScreenLogo.gif'),
+      ),
+    );
+  }
+}
